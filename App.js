6f8ab0509a14c4dce4dc4fe608764c35e6e5b618
olympia <owojcik7@gmail.com>
2019-12-07 17:50:48 -0500

    Get started with inline styles on View and TextInput

--- App.js
+++ App.js
@@ -1,19 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <View >
+      <View style={{ padding: 50 }}>
+        <TextInput placeholder="Course Goal" style={{ borderBottomColor: 'black', borderBottomWidth: 1, padding: 10 }} />
+        <Button title="Add" />
+      </View>
+      <View>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    </View >
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+
 });
