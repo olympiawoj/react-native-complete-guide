de29b3e1768a459306805eceae8a3ff74da07677
olympia <owojcik7@gmail.com>
2019-12-07 20:09:56 -0500

    Work with state & events to manage list of goals

--- App.js
+++ App.js
@@ -1,20 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, TextInput, Button } from 'react-native';
 
 export default function App() {
+  const [enteredGoal, setEnteredGoal] = useState('')
+  const [courseGoals, setCourseGoals] = useState([])
+
+  const goalInputHandler = (enteredText) => {
+    setEnteredGoal(enteredText)
+  }
+
+  const addGoalHandler = () => {
+    //functional form of updateState to updateState based on prev
+    setCourseGoals(currentGoals => [...currentGoals, enteredGoal])
+  }
+
   return (
-    <View >
-      <View style={{ padding: 50 }}>
-        <TextInput placeholder="Course Goal" style={{ borderBottomColor: 'black', borderBottomWidth: 1, padding: 10 }} />
-        <Button title="Add" />
+    <View style={styles.screen}>
+      <View style={styles.inputContainer}>
+        <TextInput placeholder="Course Goal" style={styles.input} onChangeText={goalInputHandler} value={enteredGoal} />
+        <Button title="Add" onPress={addGoalHandler} />
       </View>
       <View>
-        <Text>Open up App.js to start working on your app!</Text>
+
       </View>
     </View >
   );
 }
 
 const styles = StyleSheet.create({
-
+  screen: {
+    padding: 50
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: "space-between",
+    alignItems: 'center'
+  },
+  input: {
+    width: '80%',
+    borderColor: 'black',
+    borderWidth: 1,
+    padding: 10
+  }
 });
+
